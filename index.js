0b7af4d248ea016489cb117ccb99261c677d798f
najira <najira_daniela@hotmail.com>
2024-05-23 01:10:48 -0400

    carpetas hbs y configuracion

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import 'dotenv/config'
+import { engine } from 'express-handlebars'
 
 const app = express()
 
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`servidor escuchando en puerto ${PORT}`)
 })
+
